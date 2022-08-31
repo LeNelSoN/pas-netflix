5968480d3ccb774c237be44d38e888088fa48008
LeNelSoN <nelis.valentin@gmail.com>
2022-08-31 22:36:59 +0200

    fav add

--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
 import NavBar from "./components/NavBar/NavBar";
 import MovieCard from "./components/MovieCard/MovieCard";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Movies from "./components/Movies";
 import { useEffect, useState } from "react";
+import FavoriteContainer from "./components/FavoriteContainer/FavoriteContainer";
 
 function App() {
-  const [search, setSearch] = useState('')
-  const [movie, setMovie] = useState([])
+  const [search, setSearch] = useState("");
+  const [movie, setMovie] = useState([]);
+  const [fav, setFav] = useState([]);
 
   const getMovies = async (movieToSearch) => {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${movieToSearch}&language=fr-FR`
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=${movieToSearch}&language=fr-FR`;
     const res = await fetch(url);
-    console.log(res)
-    const {results} = await res.json()
+    console.log(res);
+    const { results } = await res.json();
     setMovie(results);
-  }
+  };
 
   useEffect(() => {
-    search && getMovies(search)
-  }, [search])
-  
+    search && getMovies(search);
+  }, [search]);
+
+ console.log(fav)
+  const MovieView = movie.map(({ id, title, poster_path, vote_average}) => (
+    <MovieCard
+      id={id}
+      key={id}
+      fav={fav}
+      setFav={setFav}
+      title={title}
+      withTitle={true}
+      withButtonFav={true}
+      vote={vote_average}
+      img={
+        poster_path === null
+          ? "https://picsum.photos/200/300/?blur=2"
+          : `https://image.tmdb.org/t/p/w500/${poster_path}`
+      }
+    />
+  ))
+
+  const favView = fav.map(({ id, img, title}) => (
+    <MovieCard
+      key={id}
+      img={img}
+      colNumber={5}
+      marge={1}
+      title={title}
+    />
+  ))
+
   return (
     <>
-      <NavBar setSearch={setSearch}/>
+      <FavoriteContainer>
+        {favView}
+      </FavoriteContainer>
+      <NavBar setSearch={setSearch} />
       <Movies>
-      {movie.map(({id,title,poster_path})=>
-        <MovieCard key={id} title={title} img={poster_path === null ? 'https://picsum.photos/200/300/?blur=2' : `https://image.tmdb.org/t/p/w500/${poster_path}`}/>
-      )}
+        {MovieView}
       </Movies>
     </>
-
   );
 }
 
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import './MovieCard.css'
+import { BsHeart } from 'react-icons/bs';
 
-function MovieCard({title ="title", img = "https://picsum.photos/200/300?random=1"}) {
+function MovieCard({title , img, vote , id, setFav, fav,colNumber = 2,marge = 3, withButtonFav = false}) {
+
+    const pillColor = vote < 5 ? vote < 2 ? 'bg-dark' : 'bg-danger' : vote > 7 ? 'bg-success' : 'bg-warning'
+
+    const handleClick = () => {
+        setFav([...fav, {img, id, title}])
+    }
+
   return (
-    <div className='col-3 m-3'>
-        <div className="card">
-        <img src={img} className="card-img" alt="..." />
-        <div className="card-img-overlay text-center">
-            {/* <h5 className="card-title">{title}</h5> */}
-        </div>
+    <div className={`col-${colNumber} m-${marge} `}>
+            <div className="card bg-dark position-relative ">
+                <img src={img} className="card-img" alt={title} />
+                <div className="card-img-overlay text-center">
+            </div>
+            <h2 className="card-title text-light text-center h6">{title}</h2>
+                <span className={`position-absolute top-0 translate-middle badge rounded-pill ${pillColor}`}>
+                {vote}
+                </span>
+                {withButtonFav && <div className='position-absolute top-0 start-100 translate-middle'>
+                    <button className='btn btn-secondary' onClick={handleClick}><BsHeart /></button>
+                </div>}
         </div>
     </div>
     )
